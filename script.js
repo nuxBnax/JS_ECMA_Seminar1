5cdefdcf1fd28e5483b92b34d30ac542449317d6
Kulikov Alexander <shouter84@yandex.ru>
2024-06-24 21:11:00 +0300

    Commit

--- script.js
+++ script.js
@@ -39,3 +39,44 @@ console.log(mergeArrays(array, array2));
 // console.log(removeDuplicates(1, 2, 3, 2, 4, 1, 5));
 
 // [1, 2, 3, 2, 4, 1, 5].filter((el, idx, arr) => arr.indexOf(el) === idx);
+
+
+// --------------------------------------------
+// Задание 2 (тайминг 25 минут)
+// 1. Напишите функцию getEvenNumbers, которая принимает массив
+// чисел в качестве аргумента и возвращает новый массив,
+// содержащий только четные числа.
+// 2. Задача: Напишите функцию calculateAverage, которая принимает
+// массив чисел в качестве аргумента и возвращает среднее значение
+// этих чисел.
+// 3. Напишите функцию capitalizeFirstLetter, которая принимает строку в
+// качестве аргумента и возвращает новую строку, в которой первая
+// буква каждого слова является заглавной.
+
+function getEvenNumbers(array) {
+    return array.filter((element) => element % 2 === 0);
+}
+console.log(getEvenNumbers([1, 2, 3, 2, 4, 1, 5]) + ' четные числа');
+
+/**
+ * 
+ * @param {Array} array - это массив :))))
+ * @returns 
+ */
+function calculateAverage(array) {
+    return array.reduce((accumulator, element) => accumulator + element, 0) / array.length;
+}
+// accumulator - собирает/аккумулирует значения в ОДНО число!!!!!
+// наш accumulator начинает с 0
+
+console.log(calculateAverage([1, 2, 3, 2, 4, 1, 5]));
+
+function capitalizeFirstLetter(str) {
+    const str1 = str.split(' ');
+    return str1.map(element => element.charAt(0).toUpperCase() + element.slice(1)).join(' ');
+}
+
+console.log(capitalizeFirstLetter('наш accumulator начинает с 0'));
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#Iteration_methods
+
